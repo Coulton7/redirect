380e42a4c73d106fd7de535f862fed2f5e794cd9
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-24 11:21:21 +0000

    Removal files that will not be needed in this theme

--- js/qt-flexslider.js
+++
@@ -1,7 +0,0 @@
-(function($){
-  $(document).ready(function() {
-    $('.location-branches .nav-tabs').children('li').find('a').on('click', function(){
-      $('.optionset-flexlsider-location').resize();
-    });
-  });
-})(jQuery);
