f828b5d3c4d67627b517445a4de71d6ec52f7e05
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-05-26 12:29:25 +0100

    Full aesseal link added also

--- templates/page--error_pages.tpl.php
+++ templates/page--error_pages.tpl.php
@@ -164,7 +164,7 @@
 		          <li> <a href="http://www.linkedin.com/company/aesseal" target="_self" title="AESEEAL Linkedin"></a></li>
 		          <li> <a href="https://www.aesseal.com/en/contact-us" target="_self" title="Contact Us"></a></li>
 		          <li> <a href="https://www.aesseal.com/en/locations" target="_self" title="Browse all Locations"></a></li>
-		          <li> <a href="/locations/europe/gb/syk/aesseal-plc-headquarters" target="_self" title="View Global Headquarters"> </a></li>
+		          <li> <a href="https://www.aesseal.com/locations/europe/gb/syk/aesseal-plc-headquarters" target="_self" title="View Global Headquarters"> </a></li>
 		        </ul>
 		      </div>
 		  	</div>
